431b1337ff7fc7149f7cfdfde4dba25478ccdf84
Italo Granato <italo.granato@gmaill.com>
2017-05-11 16:35:11 -0500

    Add marg.fun
    add marginal function to estimate h parameter

--- R/mainGE.R
+++ R/mainGE.R
@@ -129,8 +129,7 @@ getK <- function(Y, X, XF = NULL, method = c("GK", "G-BLUP"), h = NULL, model = 
            rownames(D0) <- rep(rownames(D), nEnv)
            D00 <- D0[match(Y[, 2L], rownames(D0)), match(Y[, 2L], rownames(D0))]
 
-           if (!hash) {
-             source(paste(getwd(), "/margh.fun.R", sep = "")) # calling margh.fun
+           if (is.null(h)) {
              sol <- optim(c(1, 1), margh.fun, y = Y0, D = D00, q = quantile(D00, 0.05),
                           method = "L-BFGS-B", lower = c(0.05, 0.05), upper = c(6, 30))
              h <- sol$par[1]
@@ -177,6 +176,7 @@ getK <- function(Y, X, XF = NULL, method = c("GK", "G-BLUP"), h = NULL, model = 
       stop("Model selected is not available ")
     })
 
+
   ifelse(hasXF, #Test
          return(c(list(list(X = XF, model = "FIXED")), out)), #TRUE
          return(out)) #FALSE
@@ -198,4 +198,25 @@ GEcov <- function(Y, K, nIter = 1000, burnIn = 300, thin = 5, ...)
 
   return(fit)
 }
+
+# Bandwidth parameter for bayesian kernel regression model
+margh.fun <- function(theta, y, D, q, nu=0.0001, Sc=0.0001, nuh=NULL, Sch=NULL, prior=NULL)
+{
+  h <- theta[1]
+  phi <- theta[2]
+  Kh <- exp(-h*D/q)
+  eigenKh <- eigen(Kh)
+  nr <- length(which(eigenKh$val> 1e-10))
+  Uh <- eigenKh$vec[,1:nr]
+  Sh <- eigenKh$val[1:nr]
+  d <- t(Uh) %*% scale(y, scale=F)
+
+  Iden <- -1/2*sum(log(1+phi*Sh)) - (nu+nr-1)/2*log(Sc+sum(d^2/(1+phi*Sh)))
+  if(!is.null(prior)) lprior <- dgamma(h,nuh,Sch,log=T) else lprior <- 0
+
+  Iden <- -(Iden+lprior)
+
+  return(Iden)
+}
+
 
