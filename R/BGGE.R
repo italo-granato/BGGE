85e37e890f02695def748c803ca969dd21da6b7a
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-11-17 16:09:08 -0600

    Fixed bugs
    *   Avoid 1:length()... They are error with eval the expression 1:0.

--- R/BGGE.R
+++ R/BGGE.R
@@ -140,7 +140,7 @@ BGGE <- function(y, K, XF = NULL, ne, ite = 1000, burn = 200, thin = 3, verbose 
     }
     
     
-    for (i in 1:length(K)) {
+    for (i in seq_len(K)) {
       if (K[[i]]$Type == "D") {
         tmp <- list()
         ei <- eig(K[[i]]$Kernel, tol)
@@ -245,8 +245,8 @@ BGGE <- function(y, K, XF = NULL, ne, ite = 1000, burn = 200, thin = 3, verbose 
   nu <- 3
   Sce <- (nu + 2) * (1 - R2) * var(y, na.rm = TRUE)
   Sc <- numeric(length(K))
-  for (i in 1:length(K)) {
-    Sc[i] <- (nu + 2) * R2 * var(y, na.rm = T)/mean(diag(K[[i]]$Kernel))
+  for (i in seq_len(K)) {
+    Sc[i] <- (nu + 2) * R2 * var(y, na.rm = TRUE)/mean(diag(K[[i]]$Kernel))
   }
   tau <- 0.01
   u <- list()
--- R/getK.R
+++ R/getK.R
@@ -142,7 +142,7 @@ getK <- function(Y, X, kernel = c("GK", "GB"), setKernel = NULL, bandwidth = 1, 
              D <- (as.matrix(dist(X))) ^ 2
              
              G <- list()
-             for(i in 1:length(bandwidth)){
+             for(i in seq_len(bandwidth)){
                ker.tmp <- exp(-bandwidth[i] * D / quantile(D, quantil))
                #G[[i]] <- Zg %*% tcrossprod(ker.tmp, Zg)
                G[[i]] <- list(Kernel = Zg %*% tcrossprod(ker.tmp, Zg), Type = "D")
@@ -208,7 +208,7 @@ getK <- function(Y, X, kernel = c("GK", "GB"), setKernel = NULL, bandwidth = 1, 
            
            out.tmp <- list()
            
-           for(j in 1:length(G)){
+           for(j in seq_len(G)){
            out.tmp <- c(out.tmp, lapply(1:nEnv, function(i){
              ZEE[,i] <- Ze[,i]
              ZEEZ <- ZEE %*% t(Ze)
